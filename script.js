66138c97e6136bb396f8113f015cbfa35a12a406
burtk33 <burtk33@gmail.com>
2021-01-08 19:16:18 -0500

    Adding javascript prompts for upper and lower case characters

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowercase = false;
+var uppercase = false;
 
 // Write password to the #password input
 function writePassword() {
@@ -8,6 +10,41 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+//generate password function
+function generatePassword() {
+  var passwordLength = prompt("How long should the password be?");
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password must be between 8 and 128 characters!");
+  }
+  else {
+    lowercaseModifier();
+    uppercaseModifier();
+  }
+
+}
+
+//Prompt user to include lowercase characters
+function lowercaseModifier() {
+  var includeLower = prompt("Do you want the password to include lowercase letters? (y/n)?")
+  if (includeLower === "y") {
+    lowercase = true;
+  }
+  else {
+    lowercase = false;
+  }
+}
+
+//Prompt user to include upper case characters
+function uppercaseModifier() {
+  var includeUpper = prompt("Do you want the password to include uppercase letters? (y/n)?")
+  if (includeUpper === "y") {
+    uppercase = true;
+  }
+  else {
+    uppercase = false;
+  }
 }
 
 // Add event listener to generate button
